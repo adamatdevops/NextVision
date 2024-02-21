f071202a0e741f3f58726069986047199084d4ea
adamatdevops <akeinan@surgimate.com>
2024-02-21 07:36:29 +0200

    commiting new project on main

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
+import { AppShell, Navbar, Header, MediaQuery, Burger, ActionIcon, Group, Skeleton } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+// TODO: Check whats the best icons for the dashboard
+// import { MantineLogo } from '@mantinex/mantine-logo';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { SunIcon } from '@modulz/radix-icons';
+import { MantineProvider, Text } from '@mantine/core';
+import { createStyles, useMantineTheme } from '@mantine/core';
+import { useState } from 'react';
+
+import { MemoryRouter, NavLink, Route, Routes } from 'react-router-dom';
+
+import { invoke } from '@tauri-apps/api/tauri';
+
+import Home from './Home'
+import Settings from './Settings';
+
+export function App() {
+	const [opened, { toggle }] = useDisclosure();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <MantineLogo size={30} />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+	);
 }
 
-export default App;
+// export default App;
