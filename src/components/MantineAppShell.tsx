289e029ec983f7fd8b8b9e97e13dce4986a903ae
adamatdevops <adamatdevops@gmail.com>
2024-03-20 21:28:32 +0200

    improving buttons & adding icons

--- src/components/MantineAppShell.tsx
+++ src/components/MantineAppShell.tsx
@@ -1,11 +1,24 @@
-import { AppShell, Button, Flex, Group } from '@mantine/core';
+import { AppShell, Button, Flex, Group, rem, Stack, Space } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { IconPhoto, IconDownload, IconArrowRight } from '@tabler/icons-react';
+import {
+	IconPhoto,
+	IconDashboard,
+	IconAddressBook,
+	IconShieldHalf,
+	IconNotification,
+	IconBellRinging,
+	IconDeviceDesktopAnalytics,
+	IconBrandWikipedia,
+	IconHelpOctagon,
+	IconRefresh,
+	IconUserCircle,
+	IconSettings
+} from '@tabler/icons-react';
+import classes from "./Buttons.module.css";
 
 export function MantineAppShell() {
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
 	const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
-	const icon = <IconPhoto size={14} />;
 
   return (
     <AppShell
@@ -20,38 +33,49 @@ export function MantineAppShell() {
     >
 			<AppShell.Header>
 				<Group h="100%" px="md">
-					<Button onClick={toggleDesktop} visibleFrom="sm" leftSection={icon}>
+					<Button onClick={toggleDesktop} visibleFrom="sm" leftSection={<IconPhoto size={14} />}>
         		Toggle Home View
       		</Button>
-      		<Button onClick={toggleMobile} hiddenFrom="sm" leftSection={icon}>
+      		<Button onClick={toggleMobile} hiddenFrom="sm" leftSection={<IconPhoto size={14} />}>
         		Toggle Home View
       		</Button>
 				</Group>
 
       </AppShell.Header>
 
-      <AppShell.Navbar p="0">
+      <AppShell.Navbar>
 				<Flex
       		mih={800}
-      		bg="rgba(5, 135, 235, 0.92)"
-      		gap="lg"
+      		bg="rgba(59, 129, 227, 0.92)"
+      		gap="sm"
+					rowGap="10"
       		justify="flex-start"
       		align="center"
       		direction="column"
       		wrap="wrap"
     		>
-
-      	<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={icon} p="sm">Dashboard</Button>
-      	<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={icon} p="sm">Settings</Button>
-      	<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={icon} p="sm">Accounts</Button>
-				<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={icon} p="sm">Security</Button>
-				<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={icon} p="sm">Notifications</Button>
-				<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={icon} p="sm">Alerts</Button>
-				<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={icon} p="sm">Logs & Activity</Button>
-				<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={icon} p="sm">SurgiWiki</Button>
-				<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={icon} p="sm">Open Issues</Button>
-				<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={icon} p="sm">Updates</Button>
+					<Stack
+						h={640}
+						//bg="#ffffff"
+						align="flex-start"
+						justify="space-around"
+						gap="sm"
+					>
+						<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={<IconDashboard style={{ width: rem(40), height: rem(20) }}/>} p="10" size="md" h="40" mt="md">Dashboard</Button>
+      			<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={<IconAddressBook style={{ width: rem(40), height: rem(20) }}/>} p="10" size="md" h="40">Profile</Button>
+						<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={<IconShieldHalf style={{ width: rem(40), height: rem(20) }}/>} p="10" size="md" h="40">Security</Button>
+						<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={<IconNotification style={{ width: rem(40), height: rem(20) }}/>} p="10" size="md" h="40">Notifications</Button>
+						<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={<IconBellRinging style={{ width: rem(40), height: rem(20) }}/>} p="10" size="md" h="40">Alerts</Button>
+						<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={<IconDeviceDesktopAnalytics style={{ width: rem(40), height: rem(20) }}/>} p="10" size="md" h="40">Logs & Activity</Button>
+						<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={<IconBrandWikipedia style={{ width: rem(40), height: rem(20) }}/>} p="10" size="md" h="40">SurgiWiki</Button>
+						<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={<IconHelpOctagon style={{ width: rem(40), height: rem(20) }}/>} p="10" size="md" h="40">Support</Button>
+						<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={<IconRefresh style={{ width: rem(40), height: rem(20) }}/>} p="10" size="md" h="40">Updates</Button>
+						<Space h="md" />
+						<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={<IconUserCircle style={{ width: rem(40), height: rem(20) }}/>} p="10" size="md" h="40" mt="xl">Accounts</Button>
+						<Button onClick={toggleDesktop} visibleFrom="lg" variant="outline" color="rgba(250, 245, 245, 1)" leftSection={<IconSettings style={{ width: rem(40), height: rem(20) }}/>} p="10" size="md" h="40">Settings</Button>
+					</Stack>
     	</Flex>
+
 			</AppShell.Navbar>
 
       <AppShell.Main>
