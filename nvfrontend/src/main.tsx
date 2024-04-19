d4d3575d42972a7a93fb140ac6f5efc99c6e02fd
adamatdevops <adamatdevops@gmail.com>
2024-04-19 12:38:12 +0300

    adding dist directory

--- nvfrontend/src/main.tsx
+++ nvfrontend/src/main.tsx
@@ -1,7 +1,13 @@
+/* Importing Backend */
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from '../../src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
+/* Importing UI */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
