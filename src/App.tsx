309ada7a9e1e6373a54cd23fe1605b680c479022
adamatdevops <adamatdevops@gmail.com>
2024-04-22 13:07:34 +0300

    adjusting vite mantine react environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Amplify } from "aws-amplify";
 import awsExports from "./aws-exports";
 import { useState } from 'react'
+import { Button, Flex, Heading, Image, Text } from '@aws-amplify/ui-react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '@aws-amplify/ui-react/styles.css';
+
 // import { generateClient } from "aws-amplify/api";
 // import { createTodo } from './graphql/mutations';
 // mport * as mutations from "./graphql/mutation";
@@ -12,9 +15,6 @@ import './App.css'
 import "@aws-amplify/ui-react/styles.css";
 import {
     withAuthenticator,
-    Button,
-    Heading,
-    Image,
     View,
     Card,
 } from "@aws-amplify/ui-react";
@@ -25,17 +25,38 @@ import {
 
 Amplify.configure( awsExports ); // Configure Amplify the usual way
 
-function App( signOut ) {
+function App( ) {
     const [count, setCount] = useState( 0 )
 
     return (
-        <View className="App">
-            <Card>
-                <Image src={logo} className="App-logo" alt="logo" />
-                <Heading level={1}>We now have Auth!</Heading>
-            </Card>
-            <Button onClick={signOut}>Sign Out</Button>
-        </View>
+        <Flex
+            direction={{ base: 'column', large: 'row' }}
+            maxWidth="32rem"
+            padding="1rem"
+            width="100%"
+        >
+            <Image
+                alt="Abstract art"
+                height="21rem"
+                objectFit="cover"
+                src="https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987"
+            />
+            <Flex justifyContent="space-between" direction="column">
+                <Heading level={3}>Abstract art</Heading>
+                <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat
+                    sed cras ornare arcu dui. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse.
+                </Text>
+                <Button
+                    variation="primary"
+                    onClick={() => alert( 'Added item to cart!' )}
+                >
+                    Add to Cart
+                </Button>
+            </Flex>
+        </Flex>
     );
 }
 
